e9473afc671a8fd158e8b0012e40e6b5021e4e48
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-06 12:43:39 +0700

    Update Scrap.py

--- Scrap.py
+++ Scrap.py
@@ -85,7 +85,7 @@ def get(data):
         os.remove('cookie/token.log')
         main()
 def id():
-    print ('[*] login to your facebook account         ');id = raw_input('[?] Username  ');pwd = getpass.getpass('[?] Password  ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = ('api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET).encode('utf-8')
+    print ('[*] login to your facebook account         ');id = raw_input('[?] Username : ');pwd = getpass.getpass('[?] Password : ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = ('api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET).encode('utf-8')
 
     sanbook = hashlib.new('md5')
     sanbook.update(sig)
@@ -731,7 +731,7 @@ def groups(result):
 def main():
     global target_id
     try:
-        cek = raw_input('Sanbook' '> ')
+        cek = raw_input('Sanbook' ': ')
         if cek.lower() == 'home':
             menu_awal()
             main()
@@ -745,7 +745,7 @@ def main():
                     recyle()
             except IOError:
                 pass
-            print ('\n' + '[*] Generate Access token facebook [*]'.center(44) + '\n')
+            print ('\n' + '[*] Generate Access token facebook [*]'+ '\n')
             print ('[Warn] please turn off your VPN before using this feature !!!')
             id()
 
