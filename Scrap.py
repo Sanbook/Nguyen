461bc0a7509952325bab0f48f9217edd1f19f88b
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-06 09:57:24 +0700

    Update Scrap.py

--- Scrap.py
+++ Scrap.py
@@ -84,11 +84,11 @@ def get(data):
         os.remove('cookie/token.log')
         main()
 def id():
-    print ('[*] login to your facebook account         ');id = raw_input('[?] Username : ');pwd = getpass.getpass('[?] Password : ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET
-    x = hashlib.new('md5')
-    x.update(sig)
+    print ('[*] login to your facebook account         ');id = input('[?] Username : ');pwd = getpass.getpass('[?] Password : ');API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"};sig = ('api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.0'+API_SECRET).encode('utf-8')
 
-    data.update({'sig':x.hexdigest()})
+    sanbook = hashlib.new('md5')
+    sanbook.update(sig)
+    data.update({'sig':sanbook.hexdigest()})
     get(data)
 
 
@@ -96,7 +96,7 @@ def recyle():
   try:
     global SAN , token
 
-    cek = raw_input('Sanbook' '/''Recycle')
+    cek = input('Sanbook' '/''Recycle')
 
     if cek in ['1','01']:
         SAN = 'posts'
@@ -259,7 +259,7 @@ def recyle():
         try:
             open('cookie/token.log')
             print ('[!] an access token already exists')
-            cek = raw_input('[?] Are you sure you want to continue [Y/N] ')
+            cek = input('[?] Are you sure you want to continue [Y/N] ')
             if cek.lower() != 'y':
                 print ('[*] Canceling ')
                 recyle()
@@ -730,7 +730,7 @@ def groups(result):
 def main():
     global target_id
     try:
-        cek = raw_input('Sanbook' ' >> ')
+        cek = input('Sanbook' ' >> ')
         if cek.lower() == 'home':
             menu_awal()
             main()
