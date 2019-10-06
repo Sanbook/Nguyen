150c1cb5bff58cc104775bf76fe04b21c5b7eb2a
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-10-06 11:41:14 +0700

    Update scrap.sh

--- scrap.sh
+++ scrap.sh
@@ -1,1 +1,14 @@
+# | ::::::: Fancy progressbar for lengthy operations
+# |
+if [[ ${#OPTION[@]} -gt 5 ]]; then
+  # Note: We use eval here because we want the functions to be available in this script
+  if [[ "$(uname)" = "Darwin" ]]; then
+    eval "$(curl -so- ${PROGRESS_URL})" 2> /dev/null
+  else
+    eval "$(wget -qO- ${PROGRESS_URL})"  2> /dev/null
+  fi
+fi
 
+
+
+PROGRESS_URL="https://raw.githubusercontent.com/phenonymous/shell-progressbar/1.0/progress.sh"
